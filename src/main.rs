639e98ce82422e7b9a0c11e2f6fb0ab1a7492ca0
AureliaDolo <aurelia.dolo@gmail.com>
2022-01-14 14:41:38 +0100

    Implement first example (see https://github.com/ggez/ggez/blob/master/docs/guides/HelloGgez.md)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::time::Duration;
+
+use ggez::{
+    conf,
+    event::{self, EventHandler},
+    timer, ContextBuilder, GameError,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let state = State {
+        dt: Duration::new(0, 0),
+    };
+
+    let c = conf::Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("bomberman", "Aurélia")
+        .default_conf(c)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
+}
+
+struct State {
+    dt: Duration,
+}
+
+impl EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
+        self.dt = timer::delta(ctx);
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut ggez::Context) -> Result<(), GameError> {
+        println!("Hello ggez! dt = {}ns", self.dt.as_nanos());
+        Ok(())
+    }
 }
