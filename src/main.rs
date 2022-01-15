327a9a86bacfa1ba1b1a7e2ac36b82bfede9ef48
AureliaDolo <aurelia.dolo@gmail.com>
2022-01-15 17:10:59 +0100

    The walls are real ...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use ggez::{
     conf,
     event::{self, EventHandler, KeyCode},
@@ -7,7 +9,7 @@ use ggez::{
 };
 use glam::Vec2;
 
-const DELTA: f32 = 0.2;
+const DELTA: f32 = 0.002;
 
 fn main() -> GameResult {
     let c = conf::Conf::new();
@@ -22,18 +24,26 @@ fn main() -> GameResult {
                 .flatten(),
         )
         .collect();
+    //let walls = HashSet::new();
 
     let world = World {
         height,
         width,
         walls,
     };
+    let x_grid_to_window = c.window_mode.width / world.width as f32;
+    let y_grid_to_window = c.window_mode.height / world.height as f32;
     let state = State {
-        pos_x: c.window_mode.width / 2.,
-        pos_y: c.window_mode.height / 2.,
-        x_grid_to_window: c.window_mode.width / world.width as f32,
-        y_grid_to_window: c.window_mode.height / world.height as f32,
+        pos_x: width as f32 / 2.,
+        pos_y: height as f32 / 2.,
+        x_grid_to_window,
+        y_grid_to_window,
         world,
+        player_size: if x_grid_to_window < y_grid_to_window {
+            x_grid_to_window * 0.8
+        } else {
+            y_grid_to_window * 0.8
+        },
     };
     let (ctx, event_loop) = ContextBuilder::new("bomberman", "Aurélia")
         .default_conf(c)
@@ -49,37 +59,82 @@ struct State {
     world: World,
     x_grid_to_window: f32,
     y_grid_to_window: f32,
+    player_size: f32,
 }
 
 struct World {
     height: u64,
     width: u64,
-    walls: Vec<(u64, u64)>,
+    walls: HashSet<(u64, u64)>,
 }
 
+impl State {
+    fn collision(&self, x: f32, y: f32) -> bool {
+        let borders = vec![
+            (
+                ((x * self.x_grid_to_window + self.player_size / 2.) / self.x_grid_to_window)
+                    as u64,
+                ((y * self.y_grid_to_window + self.player_size / 2.) / self.y_grid_to_window)
+                    as u64,
+            ),
+            (
+                ((x * self.x_grid_to_window + self.player_size / 2.) / self.x_grid_to_window)
+                    as u64,
+                ((y * self.y_grid_to_window - self.player_size / 2.) / self.y_grid_to_window)
+                    as u64,
+            ),
+            (
+                ((x * self.x_grid_to_window - self.player_size / 2.) / self.x_grid_to_window)
+                    as u64,
+                ((y * self.y_grid_to_window + self.player_size / 2.) / self.y_grid_to_window)
+                    as u64,
+            ),
+            (
+                ((x * self.x_grid_to_window - self.player_size / 2.) / self.x_grid_to_window)
+                    as u64,
+                ((y * self.y_grid_to_window - self.player_size / 2.) / self.y_grid_to_window)
+                    as u64,
+            ),
+        ]
+        .into_iter()
+        .collect::<HashSet<_>>();
+        self.world.walls.intersection(&borders).next().is_some()
+    }
+}
 impl EventHandler<GameError> for State {
     fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
-        if keyboard::is_key_pressed(ctx, KeyCode::Right) {
-            self.pos_x += DELTA;
-        } else if keyboard::is_key_pressed(ctx, KeyCode::Left) {
-            self.pos_x -= DELTA;
-        } else if keyboard::is_key_pressed(ctx, KeyCode::Down) {
-            self.pos_y += DELTA;
-        } else if keyboard::is_key_pressed(ctx, KeyCode::Up) {
-            self.pos_y -= DELTA;
+        let (mut new_x, mut new_y) = (self.pos_x, self.pos_y);
+        if keyboard::is_key_pressed(ctx, KeyCode::Right) && !self.collision(new_x + DELTA, new_y) {
+            new_x += DELTA
         }
+        if keyboard::is_key_pressed(ctx, KeyCode::Left) && !self.collision(new_x - DELTA, new_y) {
+            new_x -= DELTA
+        }
+        if keyboard::is_key_pressed(ctx, KeyCode::Down) && !self.collision(new_x, new_y + DELTA) {
+            new_y += DELTA
+        }
+        if keyboard::is_key_pressed(ctx, KeyCode::Up) && !self.collision(new_x, new_y - DELTA) {
+            new_y -= DELTA
+        }
+
+        self.pos_x = new_x;
+        self.pos_y = new_y;
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        let circle = graphics::Mesh::new_circle(
+        let player = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            10.0,
-            0.5,
+            Rect {
+                x: -self.player_size / 2.,
+                y: -self.player_size / 2.,
+                w: self.player_size,
+                h: self.player_size,
+            },
             Color::WHITE,
         )?;
         let brick = graphics::Mesh::new_rounded_rectangle(
@@ -104,7 +159,14 @@ impl EventHandler<GameError> for State {
                 ),),
             )?;
         }
-        graphics::draw(ctx, &circle, (Vec2::new(self.pos_x, self.pos_y),))?;
+        graphics::draw(
+            ctx,
+            &player,
+            (Vec2::new(
+                self.pos_x * self.x_grid_to_window,
+                self.pos_y * self.y_grid_to_window,
+            ),),
+        )?;
 
         graphics::present(ctx)?;
         Ok(())
