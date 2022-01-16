e9164ff1c9a0265aabb7f83764ea966559b1fcdb
AureliaDolo <aurelia.dolo@gmail.com>
2022-01-16 20:22:47 +0100

    Random start point.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use ggez::{
 };
 use glam::Vec2;
 use itertools::Itertools;
-use rand::random;
+use rand::{prelude::SliceRandom, random, thread_rng};
 
 const DELTA: f32 = 0.002;
 const BOMB_TIMEOUT: std::time::Duration = Duration::from_secs(3);
@@ -23,6 +23,13 @@ fn main() -> GameResult {
     let c = conf::Conf::new();
     let height = 11;
     let width = 11;
+    let mut rng = thread_rng();
+    let start_points = vec![
+        (1, 1),
+        (1, height - 2),
+        (width - 2, 1),
+        (width - 2, height - 2),
+    ];
     let walls = (0..11)
         .map(|i| vec![(0, i), (i, 0), (i, height - 1), (width - 1, i)])
         .flatten()
@@ -32,22 +39,31 @@ fn main() -> GameResult {
                 .flatten(),
         )
         .collect();
-    //let walls = HashSet::new();
 
-    let world = World::new(height, width, walls);
+    let world = World::new(height, width, walls, &start_points);
     let x_grid_to_window = c.window_mode.width / world.width as f32;
     let y_grid_to_window = c.window_mode.height / world.height as f32;
+    let player_size = if x_grid_to_window < y_grid_to_window {
+        x_grid_to_window * 0.8
+    } else {
+        y_grid_to_window * 0.8
+    };
+    let start = start_points.choose(&mut rng).unwrap();
+    let player = Player {
+        pos_x: start.0 as f32 + 0.5,
+        pos_y: start.1 as f32 + 0.5,
+        up: KeyCode::Up,
+        down: KeyCode::Down,
+        right: KeyCode::Right,
+        left: KeyCode::Left,
+        bomb: KeyCode::Space,
+    };
     let state = State {
-        pos_x: width as f32 / 2.,
-        pos_y: height as f32 / 2.,
         x_grid_to_window,
         y_grid_to_window,
         world,
-        player_size: if x_grid_to_window < y_grid_to_window {
-            x_grid_to_window * 0.8
-        } else {
-            y_grid_to_window * 0.8
-        },
+        player_size,
+        player,
     };
     let (ctx, event_loop) = ContextBuilder::new("bomberman", "Aurélia")
         .default_conf(c)
@@ -58,12 +74,21 @@ fn main() -> GameResult {
 }
 
 struct State {
-    pos_x: f32,
-    pos_y: f32,
+    player: Player,
     world: World,
     x_grid_to_window: f32,
     y_grid_to_window: f32,
     player_size: f32,
+}
+
+struct Player {
+    pos_x: f32,
+    pos_y: f32,
+    up: KeyCode,
+    down: KeyCode,
+    right: KeyCode,
+    left: KeyCode,
+    bomb: KeyCode,
 }
 
 struct World {
@@ -87,15 +112,19 @@ enum Content {
 struct Bonus {}
 
 impl World {
-    fn new(height: usize, width: usize, walls_set: HashSet<(usize, usize)>) -> World {
+    fn new(
+        height: usize,
+        width: usize,
+        walls_set: HashSet<(usize, usize)>,
+        start_points: &[(usize, usize)],
+    ) -> World {
         let mut walls = vec![vec![Content::Nothing; width]; height];
         for (x, y) in walls_set {
             walls[x][y] = Content::Wall
         }
-        walls[1][1] = Content::StartPoint;
-        walls[height - 2][1] = Content::StartPoint;
-        walls[1][width - 2] = Content::StartPoint;
-        walls[height - 2][width - 2] = Content::StartPoint;
+        for (x, y) in start_points {
+            walls[*y][*x] = Content::StartPoint
+        }
 
         for (x, y) in (0..width).cartesian_product(0..height) {
             if matches!(walls[y][x], Content::Nothing) && random::<f32>() < BREAKABLE_PROPORTION {
@@ -174,8 +203,8 @@ impl State {
 impl EventHandler<GameError> for State {
     fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
         // bomb management
-        if keyboard::is_key_pressed(ctx, KeyCode::Space) {
-            self.world.walls[self.pos_y as usize][self.pos_x as usize] =
+        if keyboard::is_key_pressed(ctx, self.player.bomb) {
+            self.world.walls[self.player.pos_y as usize][self.player.pos_x as usize] =
                 Content::Bomb(Instant::now())
         }
 
@@ -207,30 +236,30 @@ impl EventHandler<GameError> for State {
         }
 
         // update position
-        let (mut new_x, mut new_y) = (self.pos_x, self.pos_y);
-        if keyboard::is_key_pressed(ctx, KeyCode::Right)
+        let (mut new_x, mut new_y) = (self.player.pos_x, self.player.pos_y);
+        if keyboard::is_key_pressed(ctx, self.player.right)
             && !self.check_collision_right(new_x + DELTA, new_y)
         {
             new_x += DELTA
         }
-        if keyboard::is_key_pressed(ctx, KeyCode::Left)
+        if keyboard::is_key_pressed(ctx, self.player.left)
             && !self.check_collision_left(new_x - DELTA, new_y)
         {
             new_x -= DELTA
         }
-        if keyboard::is_key_pressed(ctx, KeyCode::Down)
+        if keyboard::is_key_pressed(ctx, self.player.down)
             && !self.check_collision_down(new_x, new_y + DELTA)
         {
             new_y += DELTA
         }
-        if keyboard::is_key_pressed(ctx, KeyCode::Up)
+        if keyboard::is_key_pressed(ctx, self.player.up)
             && !self.check_collision_up(new_x, new_y - DELTA)
         {
             new_y -= DELTA
         }
 
-        self.pos_x = new_x;
-        self.pos_y = new_y;
+        self.player.pos_x = new_x;
+        self.player.pos_y = new_y;
 
         Ok(())
     }
@@ -340,8 +369,8 @@ impl EventHandler<GameError> for State {
             ctx,
             &player,
             (Vec2::new(
-                self.pos_x * self.x_grid_to_window,
-                self.pos_y * self.y_grid_to_window,
+                self.player.pos_x * self.x_grid_to_window,
+                self.player.pos_y * self.y_grid_to_window,
             ),),
         )?;
 
